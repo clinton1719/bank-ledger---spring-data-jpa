6f050d3c9523f9d2d2632a5e997f86292b07af5c
clinton1719 <clintoncleetus17@gmail.com>
2026-02-12 09:03:00 +0530

    first commit

---
+++ ui/src/App.tsx
@@ -0,0 +1,16 @@
+import Typography from '@mui/material/Typography';
+
+const App = () => {
+  return (
+    <>
+      <Typography variant="h1" className='text-center pt-5'>
+        Learning by breaking series - Spring Data JPA
+      </Typography>
+      <Typography variant="h2" className='text-center p-5'>
+        Bank Ledger
+      </Typography>
+    </>
+  )
+}
+
+export default App

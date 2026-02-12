41179c7aaa64845a667e81af2476fe8565488626
clinton1719 <clintoncleetus17@gmail.com>
2026-02-12 09:08:20 +0530

    initial

---
+++ spring_data_jpa/src/main/java/com/clinton/bank_ledger/spring_data_jpa/BankLedgerSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.clinton.bank_ledger.spring_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankLedgerSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankLedgerSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ spring_data_jpa/src/test/java/com/clinton/bank_ledger/spring_data_jpa/BankLedgerSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinton.bank_ledger.spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankLedgerSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
